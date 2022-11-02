1948819cd7b56e5f592acbc63a8d265f5dbf6571
Iryna Voitsitska <voitsitska.pn@ucu.edu.ua>
2022-11-03 01:06:00 +0200

    funcs update + docstrings to each

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # voitsitska-iryna-lab7-task1
 do not copy!!! stick to UCU's academic integrity rules!!!
+
+there is task 1 which contains a number of functions that work with graphs :)
--- graph.py
+++ graph.py
@@ -7,8 +7,11 @@ lab 7 task 1 submission
 
 def get_graph_from_file(file_name: str) -> list:
     """
-    (str) -> (list)
     Read graph from file and return a list of edges.
+    Args:
+        file_name: a path to file
+    Returns:
+        list; list of edges
     >>> get_graph_from_file("/home/iryna/Documents/OP/w7/voitsitska-iryna-lab7-task1/data1.txt")
     [[1, 2], [3, 4], [1, 5]]
     """
@@ -19,10 +22,13 @@ def get_graph_from_file(file_name: str) -> list:
         output = [ [int(i) for i in item] for item in output]
     return output
 
-def to_edge_dict(edge_list):
+def to_edge_dict(edge_list: list) -> dict:
     """
-    (list) -> (dict)
     Convert a graph from list of edges to dictionary of vertices.
+    Args:
+        edge_list: list of edges
+    Returns:
+        dict: dict that represents a graph
     >>> to_edge_dict([[1, 2], [3, 4], [1, 5], [2, 4]])
     {1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}
     """
@@ -34,19 +40,27 @@ def to_edge_dict(edge_list):
         output[item[1]].insert(0, item[0])
     return output
 
-def is_edge_in_graph(graph, edge):
+def is_edge_in_graph(graph: dict, edge: tuple) -> bool:
     """
-    (dict, tuple) -> bool
     Return True if graph contains a given edge and False otherwise.
+    Args:
+        graph: a graph
+        edge: edge to check
+    Returns:
+        bool: if the graph contains the edge
     >>> is_edge_in_graph({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (3, 1))
     False
     """
     return edge in [(key, val) for key, value in graph.items() for val in value]
 
-def add_edge(graph, edge):
+def add_edge(graph: dict, edge: tuple) -> dict:
     """
-    (dict, tuple) -> dict 
     Add a new edge to the graph and return new graph.
+    Args:
+        graph: a graph
+        edge: edge to add
+    Returns:
+        the graph w the edge
     >>> add_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (1, 3))
     {1: [2, 5, 3], 2: [1, 4], 3: [4, 1], 4: [2, 3], 5: [1]}
     """
@@ -54,10 +68,14 @@ def add_edge(graph, edge):
     graph[edge[1]].append(edge[0])
     return graph
 
-def del_edge(graph, edge):
+def del_edge(graph: dict, edge: tuple) -> dict:
     """
-    (dict, tuple) -> (dict)
     Delete an edge from the graph and return a new graph.
+    Args:
+        graph: a graph
+        edge: edge to delete
+    Returns:
+        the graph w/out the edge
     >>> del_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (2, 4))
     {1: [2, 5], 2: [1], 3: [4], 4: [3], 5: [1]}
     """
@@ -65,31 +83,56 @@ def del_edge(graph, edge):
     graph[edge[1]].remove(edge[0])
     return graph
 
-def add_node(graph, node):
+def add_node(graph: dict, node: int) -> dict:
     """
-    (dict, int) -> (dict)
     Add a new node to the graph and return a new graph.
+    Args:
+        graph: dict that represents a graph
+        node: node to add
+    Returns:
+        dict: the graph w node
     >>> add_node({1: [2], 2: [1]}, 3)
     {1: [2], 2: [1], 3: []}
     """
     graph[node] = []
     return graph
 
-def del_node(graph, node):
+def del_node(graph: dict, node: int) -> dict:
     """
-    (dict, int) -> (dict)
     Delete a node and all incident edges from the graph.
+    Args:
+        graph: dict that represents a graph
+        node: int, node to delete
+    Returns:
+        dict: dict w/out node
     >>> del_node({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, 4)
     {1: [2, 5], 2: [1], 3: [], 5: [1]}
     """
-    pass
+    for value in graph.values():
+        if node in value:
+            value.remove(node)
+    try:
+        del graph[node]
+        return graph
+    except KeyError:
+        return graph
 
-def convert_to_dot(graph):
+def convert_to_dot(graph: dict) -> None:
     """
-    (dict) -> (None)
+    Args:
+        graph: dictionary that represents a graph
+    Returns:
+        the function returns None
     Save the graph to a file in a DOT format.
+    >>> convert_to_dot({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]})
     """
-    
+    with open('w7/voitsitska-iryna-lab7-task1/graph.DOT', 'w', encoding = "utf-8") as file:
+        dotlines = "digraph {\n"
+        for key, value in graph.items():
+            for val in value:
+                dotlines += f"{key} -> {val}\n"
+        dotlines += "}"
+        file.write(dotlines)
 
 if __name__ == "__main__":
     import doctest
